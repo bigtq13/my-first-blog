bb1e48b14a5a924ad3f9d31f2e99f67214323b62
Tabitha <bigtq13@hotmail.com>
2017-08-03 14:03:03 -0700

    with the static file pattern

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.homepage, name='homepage'),
+    url(r'^index.html$',views.homepage, name='homepage'),
+    url(r'^projects/eipT.html$',views.eipT, name='eipT'),
+    url(r'^projects/chtgstoreT.html$',views.chtgstoreT, name='chtgstoreT'),
+    url(r'^projects/chtselfserviceT.html$',views.chtselfserviceT, name='chtselfserviceT'),
+    url(r'^projects/amrtT.html$',views.amrtT, name='amrtT'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'blog/index.html', {})
+
+def eipT(request):
+    return render(request, 'blog/projects/eipT.html', {})
+
+def chtgstoreT(request):
+    return render(request, 'blog/projects/chtgstoreT.html', {})
+
+def chtselfserviceT(request):
+    return render(request, 'blog/projects/chtselfserviceT.html', {})
+
+def amrtT(request):
+    return render(request, 'blog/projects/amrtT.html', {})
